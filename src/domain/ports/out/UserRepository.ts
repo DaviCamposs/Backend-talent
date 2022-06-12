385260b707047d997fd6a0add65951bbd27e07f2
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 13:59:22 -0300

    feat: method to find user by id

--- src/domain/ports/out/UserRepository.ts
+++ src/domain/ports/out/UserRepository.ts
@@ -3,4 +3,5 @@ import { User } from "../../models/User"
 export interface UserRepository {
     save(name: string, email: string, password: string): Promise<void>
     findByEmail(email: string): Promise<User>
+    findById(id: number): Promise<User>
 }
