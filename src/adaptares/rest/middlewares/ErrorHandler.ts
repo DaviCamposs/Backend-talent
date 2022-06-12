42227f70b73d808f11442cbcc4f6ea8b89944485
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 15:52:48 -0300

    feat: ErrorHandler

---
+++ src/adaptares/rest/middlewares/ErrorHandler.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import { EmailAlreadyRegisteredError } from "../../../domain/errors/EmailAlreadyRegisteredError";
+import { InvalidEmailError } from "../../../domain/errors/InvalidEmailError";
+import { NameTooShortError } from "../../../domain/errors/NameTooShortError";
+import { PasswordTooShortError } from "../../../domain/errors/PasswordTooShortError";
+import { WrongCredentialsError } from "../../../domain/errors/WrongCredentialsError";
+import { ValidationError } from "../controllers/errors/ValidationError";
+
+export function errorHandler(error: Error, request: Request, response: Response, next: NextFunction) {
+
+    if (error instanceof WrongCredentialsError) {
+        return response.status(403).json({
+            title: 'Not authorized',
+            message: 'Verify your credentials'
+        })
+    } else if (error instanceof PasswordTooShortError ||
+        error instanceof InvalidEmailError ||
+        error instanceof NameTooShortError ||
+        error instanceof EmailAlreadyRegisteredError ||
+        error instanceof ValidationError) {
+        return response.status(400).json({
+            title: 'Validation error',
+            message: 'Request body is invalid'
+        })
+    } else {
+        response.status(500).json({
+            title: "InternalServerError",
+            message: "Something went wrong",
+        })
+    }
+}
