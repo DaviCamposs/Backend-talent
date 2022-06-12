cf310fafded2de8de40b85a8311bbdbbfb793292
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 17:34:27 -0300

    refactor: remove unused imports

--- src/adaptares/rest/Application.ts
+++ src/adaptares/rest/Application.ts
@@ -2,15 +2,12 @@ import express from "express";
 import cors from 'cors';
 import { errorHandler } from "./middlewares/ErrorHandler";
 import { Route } from "./routes/Route";
-import { createConnection } from "typeorm";
-import { UserEntity } from "../repositories/entities/User";
 
 export class Application {
     public app: express.Application = express();
 
     constructor(private routeList: Route[]) {
         this.appConfig();
-        this.databaseConfig()
         this.mountRoutes();
     }
 
@@ -26,19 +23,6 @@ export class Application {
     private appConfig(): void {
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
-    }
-
-    private async databaseConfig() {
-        await createConnection({
-            type: "mysql",
-            host: "localhost",
-            port: 33066,
-            username: "root",
-            password: "root",
-            database: "talent",
-            entities: [UserEntity],
-            synchronize: true
-        })
     }
 
     startServerOnPort(port: number): void {
