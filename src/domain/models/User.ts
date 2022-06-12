8df449a4e223748e7629c7110d46dcbe2022223d
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 13:11:20 -0300

    feat: should throw an error if email is invalid

--- src/domain/models/User.ts
+++ src/domain/models/User.ts
@@ -1,3 +1,4 @@
+import { InvalidEmailError } from "../errors/InvalidEmailError"
 import { NameTooShortError } from "../errors/NameTooShortError"
 import { PasswordTooShortError } from "../errors/PasswordTooShortError"
 
@@ -7,14 +8,23 @@ export class User {
     public email: string
     public password: string
 
-    constructor (name: string, email: string , password: string) {
+    constructor(name: string, email: string, password: string) {
         if (name.length < 2)
             throw new NameTooShortError()
 
         if (password.length < 8)
             throw new PasswordTooShortError()
+
+        if (!this.validateEmail(email))
+            throw new InvalidEmailError()
+
         this.name = name
         this.email = email
         this.password = password
+    }
+
+    private validateEmail(value: string): boolean {
+        let regexValidEmail = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')
+        return regexValidEmail.test(value)
     }
 }
