b7897ef7e52ad6592d0c143d3a314a2a8ebe223e
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 13:14:58 -0300

    refactor: extract values to constant

--- test/unit/domain/models/User.test.ts
+++ test/unit/domain/models/User.test.ts
@@ -3,19 +3,26 @@ import { NameTooShortError } from "../../../../src/domain/errors/NameTooShortErr
 import { PasswordTooShortError } from "../../../../src/domain/errors/PasswordTooShortError"
 import { User } from "../../../../src/domain/models/User"
 
+const VALID_NAME = 'David'
+const INVALID_NAME = 'D'
+const VALID_PASSWORD = '12345678'
+const INVALID_PASSWORD = '12345'
+const VALID_EMAIL = 'david@mail.com'
+const INVALID_EMAIL = 'david@'
+
 describe('User Tests', () => {
     it('should create an instance of User class', () => {
-        expect(new User('david','david@mail.com', '12345678')).toBeInstanceOf(User)
+        expect(new User(VALID_NAME,VALID_EMAIL, VALID_PASSWORD)).toBeInstanceOf(User)
     })
     it('should throw an Error if the name has less than 2 chars', () => {
-        expect(() => new User('D','david@mail.com','12345678')).toThrow(NameTooShortError)
+        expect(() => new User(INVALID_NAME,VALID_EMAIL, VALID_PASSWORD)).toThrow(NameTooShortError)
     })
 
     it('should throw an error if password has less than 8 chars', () => {
-        expect(() => new User('David','david@mail.com','123456')).toThrow(PasswordTooShortError)
+        expect(() => new User(VALID_NAME,VALID_EMAIL,INVALID_PASSWORD)).toThrow(PasswordTooShortError)
     })
 
     it('should throw an Error if email is invalid', () => {
-        expect(() => new User('David','davi@','12345678')).toThrow(InvalidEmailError)
+        expect(() => new User(VALID_NAME,INVALID_EMAIL,VALID_PASSWORD)).toThrow(InvalidEmailError)
     })
 })
