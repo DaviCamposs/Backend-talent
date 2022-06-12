83229f999ced31b582e006b6ea634a01e252a3d3
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 13:38:23 -0300

    feat: save a user in useCase

--- src/domain/useCases/RegisterUser/RegisterUserUseCase.ts
+++ src/domain/useCases/RegisterUser/RegisterUserUseCase.ts
@@ -1,5 +1,19 @@
+import { EmailAlreadyRegisteredError } from "../../errors/EmailAlreadyRegisteredError";
 import { RegisterUser } from "../../ports/in/RegisterUser";
+import { EncryptService } from "../../ports/out/EncryptService";
+import { UserRepository } from "../../ports/out/UserRepository";
 
 export class RegisterUserUseCase implements RegisterUser {
-    async register(name: string, email: string, password: string): Promise<void> { }
+    constructor(private userRepository: UserRepository, private encryptService: EncryptService) { }
+
+    async register(name: string, email: string, password: string): Promise<void> {
+        const registeredUser = await this.userRepository.findByEmail(email)
+
+        if (registeredUser)
+            throw new EmailAlreadyRegisteredError()
+
+        const hashedPassword = await this.encryptService.hash(password)
+
+        await this.userRepository.save(name, email, hashedPassword)
+    }
 }
