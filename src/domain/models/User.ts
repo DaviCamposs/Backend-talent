9333e6c86aa041f35c0ac497b35e30caecc95f61
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 12:46:34 -0300

    feat: throw an error if password is invalid

--- src/domain/models/User.ts
+++ src/domain/models/User.ts
@@ -1,4 +1,5 @@
 import { NameTooShortError } from "../errors/NameTooShortError"
+import { PasswordTooShortError } from "../errors/PasswordTooShortError"
 
 export class User {
     public id?: number
@@ -9,6 +10,9 @@ export class User {
     constructor (name: string, email: string , password: string) {
         if (name.length < 2)
             throw new NameTooShortError()
+
+        if (password.length < 8)
+            throw new PasswordTooShortError()
         this.name = name
         this.email = email
         this.password = password
