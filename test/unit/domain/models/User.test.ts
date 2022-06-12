d9f6c7139e0b270f95f4e5918054395f5a6e9001
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 12:43:45 -0300

    test: throw an error if name is invalid

--- test/unit/domain/models/User.test.ts
+++ test/unit/domain/models/User.test.ts
@@ -1,7 +1,11 @@
+import { NameTooShortError } from "../../../../src/domain/errors/NameTooShortError"
 import { User } from "../../../../src/domain/models/User"
 
 describe('User Tests', () => {
     it('should create an instance of User class', () => {
         expect(new User('david','david@mail.com', '12345678')).toBeInstanceOf(User)
+    })
+    it('should throw an Error if the name has less than 2 chars', () => {
+        expect(() => new User('D','david@mail.com','12345678')).toThrow(NameTooShortError)
     })
 })
