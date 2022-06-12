4c5c5ec90e4436ac95a340390fccef36202cf090
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 12:46:39 -0300

    test: throw an error if password is invalid

--- test/unit/domain/models/User.test.ts
+++ test/unit/domain/models/User.test.ts
@@ -1,4 +1,5 @@
 import { NameTooShortError } from "../../../../src/domain/errors/NameTooShortError"
+import { PasswordTooShortError } from "../../../../src/domain/errors/PasswordTooShortError"
 import { User } from "../../../../src/domain/models/User"
 
 describe('User Tests', () => {
@@ -7,5 +8,9 @@ describe('User Tests', () => {
     })
     it('should throw an Error if the name has less than 2 chars', () => {
         expect(() => new User('D','david@mail.com','12345678')).toThrow(NameTooShortError)
+    })
+
+    it('should throw an error if password has less than 8 chars', () => {
+        expect(() => new User('David','david@mail.com','123456')).toThrow(PasswordTooShortError)
     })
 })
